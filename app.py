95258771c38e2dd0e43a96c3096d6ee731ac21db
oyaromadebright01 <oyaromadebright@gmail.com>
2026-03-08 16:32:35 +0100

    Add logout functionality and enhance user interface with new styles and features

--- app.py
+++ app.py
@@ -12,4 +12,11 @@ def register():
 def welcome():
     return render_template('welcome.html')
 
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+
+
+
 app.run(debug=True, host='0.0.0.0', port=8080)
