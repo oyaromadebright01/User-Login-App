3920af2282b46e0a0d0587205c5e6226e5da6f87
oyaromadebright01 <oyaromadebright@gmail.com>
2026-03-14 16:02:23 +0100

    user app

--- app.py
+++ app.py
@@ -1,12 +1,64 @@
+import sqlite3
 from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route('/login')
-def index():
-    return render_template('login.html')
 
-@app.route('/register')
+def get_db():
+    return sqlite3.connect('database.db')
+
+def create_table():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        )
+    ''')
+    db.commit()
+    db.close()
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST':
+        username = username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+        account = cursor.fetchone()
+        if account:
+            msg ='logged in successfully!'
+            name = account[1]
+            return render_template('welcome.html', name=name)
+        else: 
+            msg = 'Incorrect username/password!'
+        db.close()
+    return render_template('login.html', msg=msg)
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        else:
+            cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+            db.commit()
+            msg = 'You have successfully registered!'
+            return render_template('welcome.html', name=username)
+        
+        db.close()
+    return render_template('register.html', msg=msg)
 
 @app.route('/welcome')
 def welcome():
