3404d6e9b4bce08bba8bac3b52947b30dae753a7
oyaromadebright01 <oyaromadebright@gmail.com>
2026-03-15 16:03:02 +0100

    user app

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import sqlite3
+import mysql.connector
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def get_db():
-    return sqlite3.connect('database.db')
+    return mysql.connector.connect(
+        host='sql7.freesqldatabase.com',
+        user='	sql7820090',
+        password='QG1bsZdiYX',
+        database=' sql7820090'
 
+    )
+    
 def create_table():
     db = get_db()
     cursor = db.cursor()
@@ -28,7 +35,7 @@ def login():
         password = request.form['password']
         db = get_db()
         cursor = db.cursor()
-        cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+        cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
         account = cursor.fetchone()
         if account:
             msg ='logged in successfully!'
@@ -47,12 +54,12 @@ def register():
         password = request.form['password']
         db = get_db()
         cursor = db.cursor()
-        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists!'
         else:
-            cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+            cursor.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (username, password))
             db.commit()
             msg = 'You have successfully registered!'
             return render_template('welcome.html', name=username)
